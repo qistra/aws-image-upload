c1988a73c5350b602e34b8cd4aef240c35dff13c
Qi <>
2020-04-25 18:53:05 +0200

    remove unused code

--- src/main/java/com/example/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/example/awsimageupload/config/AmazonConfig.java
@@ -8,10 +8,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-//@PropertySource("application.properties")
 public class AmazonConfig {
 
     @Value("${aws.accessKey}")
