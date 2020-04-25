f07c9bc6915190af6de8a00196ca25d08093663a
Qi <>
2020-04-25 18:54:01 +0200

    add a rest controller to handle api

---
+++ src/main/java/com/example/awsimageupload/controllers/UserProfileController.java
@@ -0,0 +1,18 @@
+package com.example.awsimageupload.controllers;
+
+import com.example.awsimageupload.profile.UserProfile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user-profile")
+public class UserProfileController {
+
+    @GetMapping
+    public List<UserProfile> getUserProfiles() {
+        return null;
+    }
+}
