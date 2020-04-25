5d6e03dd11d289828229b7bb82ebb446fbf794ec
Qi <>
2020-04-25 19:21:54 +0200

    explicitly specify the region for AWS to fix the problem that no region is found

--- src/main/java/com/example/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/example/awsimageupload/config/AmazonConfig.java
@@ -3,6 +3,7 @@ package com.example.awsimageupload.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,6 +26,7 @@ public class AmazonConfig {
 
         return AmazonS3ClientBuilder
                 .standard()
+                .withRegion(Regions.DEFAULT_REGION)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
