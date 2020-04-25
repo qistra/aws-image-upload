1955cda2d5c7e665f58de7c73f35521bc23b892b
Qi <>
2020-04-25 18:30:19 +0200

    add UserProfile model

---
+++ src/main/java/com/example/awsimageupload/profile/UserProfile.java
@@ -0,0 +1,34 @@
+package com.example.awsimageupload.profile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class UserProfile {
+    private UUID userId;
+    private String userName;
+    private String userProfileImageLink; // Amazon S3
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(userName, that.userName) &&
+                Objects.equals(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName, userProfileImageLink);
+    }
+}
